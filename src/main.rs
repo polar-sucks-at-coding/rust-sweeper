d1dd6ebaa2059345e15b76370dbfba32f85fe0ee
polar-sucks-at-coding <melihekinci@proton.me>
2026-08-25 04:35:16 +0300

    added user input functionality including mouse stuff

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate pancurses;
 
-use pancurses::{initscr, endwin, noecho, Window, Input};
+use pancurses::{initscr, endwin, Window, Input, ALL_MOUSE_EVENTS, getmouse, mousemask};
 use rand::Rng;
 
 const ROWS: i32 = 5;
@@ -14,10 +14,19 @@ enum TileType{
     Empty
 }
 struct Tile{
-    position: (i32, i32), //row/y first
+    position: (i32, i32), // row/y first
     tile_type: TileType,
     concealed: bool
 
+}
+
+impl Tile{
+    fn get_symbol(&self) -> char{
+        match self.tile_type{
+            TileType::Bomb => { return BOMB_TILE_SYMBOL; },
+            TileType::Empty => { return EMPTY_TILE_SYMBOL; }
+        }
+    }
 }
 
 fn create_tiles() -> Vec<Tile>{
@@ -54,35 +63,57 @@ fn create_tiles() -> Vec<Tile>{
 
 }
 
-fn print_tiles(tiles: &Vec<Tile>, window: &mut Window, y_offset: i32, x_offset: i32){
+fn print_tiles(tiles: &Vec<Tile>, window: &mut Window, y_offset: &i32, x_offset: &i32){
     for i in tiles{
         window.mv(i.position.0 + y_offset, i.position.1 + x_offset);
 
         if i.concealed{
             window.printw(String::from(CONCEALED_TILE_SYMBOL));
-        } 
-        else{
-            match i.tile_type{
-                TileType::Bomb =>{
-                    window.printw(String::from(BOMB_TILE_SYMBOL));
-                }
-                TileType::Empty =>{
-                    window.printw(String::from(EMPTY_TILE_SYMBOL));
-                }
-            }
+            return;
         }
+
+        window.printw(i.get_symbol().to_string());
     }
 }
 
+fn print_tile_coordinates(window: &mut Window, y_offset: &i32, x_offset: &i32){
+    window.mv(y_offset - 2, *x_offset);
+    for i in 0..COLUMNS{
+        window.printw(i.to_string());
+    }
 
-fn get_input() -> String{
-    let mut input = String::new();
+    window.mv(*y_offset, x_offset - 1);
+    for i in 0..ROWS{
+        window.mv((*y_offset) + i, x_offset - 2);
+        window.printw(i.to_string());
+    }
+}
 
-    std::io::stdin().read_line(&mut input).expect("Failed to read line");
+enum InputType{
+    Quit,
+    Character(char),
+    Mouse(i32, i32) //y and x coordinates
+}
+fn get_input(window: &mut Window) -> Option<InputType>{
+        match window.getch(){
+            Some(Input::Character(c)) => {
+                if c == 'q' { Some(InputType::Quit); }
+                Some(InputType::Character(c))
+            },
 
-    let trimmed = String::from(input.trim());
+            //the delete key
+            Some(Input::KeyDC) => Some(InputType::Quit),
+            
+            Some(Input::KeyMouse) => {
+                if let Ok(mouse_event) = getmouse() { Some(InputType::Mouse(mouse_event.y, mouse_event.x)) }
+                else { None }
+            }
 
-    trimmed
+            _ => None
+            
+            //Some(input) => { window.addstr(&format!("{:?}", input)); },
+            //None => ()
+        }
 }
 
 fn conceal_all_tiles(tiles: &mut Vec<Tile>){
@@ -91,35 +122,57 @@ fn conceal_all_tiles(tiles: &mut Vec<Tile>){
     }
 }
 
-fn use_char_input(tiles: &mut Vec<Tile>, window: &mut Window, c: &char){
-    match c{
-        'c' => { conceal_all_tiles(tiles) },
-        _ => ()
-    }
-}
+//wip
+fn print_manual(window: &mut Window){
+    window.printw("hi");
+}   
 
 fn main() {
     let mut window = initscr();
     window.keypad(true);
+    mousemask(ALL_MOUSE_EVENTS, None); //for pancurses to listen to all mouse events
 
-    let mut tiles = create_tiles();
+    let tiles = create_tiles();
     let y_offset = 10;
     let x_offset = 20;
 
+    let mut str_for_debugging = String::new();
+
+    //main loop
     loop{
         window.erase();
-        print_tiles(&tiles, &mut window, y_offset, x_offset);
+        
+        //moving into position for the manual
+        window.mv(y_offset - 5, 0);
+        print_manual(&mut window);
 
-        //moving to where input will be echoed
-        window.mv(y_offset + ROWS + 2, x_offset);
+        print_tiles(&tiles, &mut window, &y_offset, &x_offset);
+        print_tile_coordinates(&mut window, &y_offset, &x_offset);
 
-        match window.getch(){
-            Some(Input::Character(c)) => { use_char_input(&mut tiles, &mut window, &c); },
-            Some(Input::KeyDC) => break,
-            Some(input) => { window.addstr(&format!("{:?}", input)); },
-            None => ()
+        window.mvprintw(30, 30, &str_for_debugging);
+
+        match get_input(&mut window){
+            Some(InputType::Character(c)) => {
+                //moving to where input will be echoed
+                str_for_debugging = c.to_string();
+            }
+
+            Some(InputType::Mouse(y, x)) => {
+                let mut stringy_pingy = String::from("Mouse at ");
+                stringy_pingy += &y.to_string()[..];
+                stringy_pingy += " ";
+                stringy_pingy += &x.to_string()[..];
+                str_for_debugging = stringy_pingy;
+            }
+
+            Some(InputType::Quit) => break,
+            Option::None => {
+                endwin();
+                eprintln!("Invalid Input!");
+                std::process::exit(1);
+            }
         }
+        window.refresh();
     }
-
     endwin();
 }
