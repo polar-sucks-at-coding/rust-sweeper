c1bba347e6673be6591d9fd53f4b74ff0fa3af4a
polar-sucks-at-coding <melihekinci@proton.me>
2026-08-28 17:42:33 +0300

    made it so tiles get the numbers they should depending on how many mines are around them

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,93 @@
 extern crate pancurses;
 
-use std::ops::Add;
-
 use pancurses::{ALL_MOUSE_EVENTS, Input::{self, KeyReference}, Window, endwin, getmouse, initscr, mousemask};
 use rand::Rng;
 
-const ROWS: usize = 5;
-const COLUMNS: usize = 5;
+const ROWS: usize = 10;
+const COLUMNS: usize = 10;
 const QUIT_BUTTON: char = 'q';
 const CONCEALED_TILE_SYMBOL: char = 'c';
 const BOMB_TILE_SYMBOL: char = 'b';
 const EMPTY_TILE_SYMBOL: char = 'e';
 const DEBUG_COORDINATES: (u32, u32) = (30, 30);
+const Y_OFFSET: i32 = 10;
+const X_OFFSET: i32 = 20;
 
+#[derive(PartialEq)]
 enum TileType{
     Bomb,
     Empty
 }
 
-enum TileClickResult{
-    Explode,
-    Safe
-}
 struct Tile{
     position: (usize, usize), // row/y first
     tile_type: TileType,
-    concealed: bool
-
+    concealed: bool,
+    symbol: char
 }
 
 impl Tile{
     fn get_symbol(&self) -> char{
-        match self.tile_type{
-            TileType::Bomb => { return BOMB_TILE_SYMBOL; },
-            TileType::Empty => { return EMPTY_TILE_SYMBOL; }
+        self.symbol
+    }
+
+    fn get_type(&self) -> &TileType{
+        &self.tile_type
+    }
+
+    fn get_surrounding_tiles<'a>(&self, tiles: &'a Vec<Tile>) -> Result<Vec<&'a Tile>, String>{
+        let mut surrounding_tiles: Vec<&'a Tile> = Vec::new();
+
+        for row in -1..2{
+            for column in -1..2{
+                if row == 0 && column == 0{
+                    continue;
+                }
+
+                let coordinates_for_surrounding_tile: (i32, i32) = (self.position.0 as i32 + row, self.position.1 as i32 + column);
+                match get_tile_from_coordinates(tiles, coordinates_for_surrounding_tile.0, coordinates_for_surrounding_tile.1){
+                    Ok(tile) => { surrounding_tiles.push(tile); }
+                    _ => {}
+                }
+            }
+        }
+
+        if surrounding_tiles.is_empty(){
+            return Err("Error: No surrounding tiles found".to_string());
+        }
+
+        return Ok(surrounding_tiles);
+    }
+
+    fn get_surrounding_bomb_count<'a>(&self, tiles: &'a Vec<Tile>) -> Option<i32>{
+        let surrounding_tiles = self.get_surrounding_tiles(tiles).ok()?;
+        let mut surrounding_bomb_count: i32 = 0;
+        
+        for i in surrounding_tiles{
+            if let TileType::Bomb = i.get_type(){
+                surrounding_bomb_count += 1;
+            }
+        }
+
+        if surrounding_bomb_count < 1{
+            return None;
+        }
+
+        Some(surrounding_bomb_count)
+    }
+
+    fn calculate_symbol(&self, tiles: &Vec<Tile>) -> char {
+        if self.get_type() == &TileType::Bomb {
+            return BOMB_TILE_SYMBOL;
+        }
+
+        match self.get_surrounding_bomb_count(tiles) {
+            Some(count) => std::char::from_digit(count as u32, 10)
+                .unwrap_or(EMPTY_TILE_SYMBOL),
+            None => EMPTY_TILE_SYMBOL,
         }
     }
-    
-    fn get_clicked(&self) -> Option<TileClickResult>{
-        match self.tile_type{
-            TileType::Bomb => Some(TileClickResult::Explode),
-            TileType::Empty => Some(TileClickResult::Safe)
-        }
-    }
+
 }
 
 fn create_tiles() -> Vec<Tile>{
@@ -54,10 +99,11 @@ fn create_tiles() -> Vec<Tile>{
         let mut tile = Tile{
             position: (row, column),
             tile_type: TileType::Empty,
-            concealed: false
+            concealed: false,
+            symbol:EMPTY_TILE_SYMBOL //temporary, will be assigned later
         };
 
-        if (rand::thread_rng().gen_range(0..2)) == 1{
+        if (rand::thread_rng().gen_range(0..5)) == 1{
             tile.tile_type = TileType::Bomb;
         }
 
@@ -79,9 +125,9 @@ fn create_tiles() -> Vec<Tile>{
 
 }
 
-fn print_tiles(tiles: &Vec<Tile>, window: &mut Window, y_offset: i32, x_offset: i32){
+fn print_tiles(tiles: &Vec<Tile>, window: &mut Window){
     for i in tiles{
-        window.mv(i.position.0 as i32 + y_offset, i.position.1 as i32 + x_offset);
+        window.mv(i.position.0 as i32 + Y_OFFSET, i.position.1 as i32 + X_OFFSET);
 
         if i.concealed{
             window.addch(CONCEALED_TILE_SYMBOL);
@@ -92,15 +138,15 @@ fn print_tiles(tiles: &Vec<Tile>, window: &mut Window, y_offset: i32, x_offset: 
     }
 }
 
-fn print_tile_coordinates(window: &mut Window, y_offset: i32, x_offset: i32){
-    window.mv(y_offset - 2, x_offset);
+fn print_tile_coordinates(window: &mut Window){
+    window.mv(Y_OFFSET - 2, X_OFFSET);
     for i in 0..COLUMNS{
         window.printw(i.to_string());
     }
 
-    window.mv(y_offset, x_offset - 1);
+    window.mv(Y_OFFSET, X_OFFSET - 1);
     for i in 0..ROWS{
-        window.mv((y_offset) + i as i32, x_offset - 2);
+        window.mv((Y_OFFSET) + i as i32, X_OFFSET - 2);
         window.printw(i.to_string());
     }
 }
@@ -110,6 +156,7 @@ enum InputType{
     Character(char),
     Mouse(i32, i32) //y and x coordinates
 }
+
 fn get_input(window: &mut Window) -> Result<InputType, String>{
         match window.getch(){
             Some(Input::Character(c)) => {
@@ -146,13 +193,13 @@ fn print_manual(window: &mut Window){
     window.printw(to_print);
 }   
 
-fn get_tile_from_coordinates(tiles: &Vec<Tile>, y_mouse: i32, x_mouse: i32, y_offset: i32, x_offset: i32) -> Result<&Tile, String>{
-    let max_coordinates = (ROWS as i32 + y_offset, COLUMNS as i32 + x_offset);
-    if x_mouse < x_offset || y_mouse < y_offset  ||  y_mouse > max_coordinates.0 || x_mouse > max_coordinates.1{
+fn get_tile_from_mouse_coordinates(tiles: &Vec<Tile>, mouse_y: i32, mouse_x: i32) -> Result<&Tile, String>{
+    let max_coordinates = (ROWS as i32 + Y_OFFSET, COLUMNS as i32 + X_OFFSET);
+    if mouse_x < X_OFFSET || mouse_y < Y_OFFSET  ||  mouse_y > max_coordinates.0 || mouse_x > max_coordinates.1{
         return Err("Error: x or y out of bounds".to_string());
     }
 
-    let index = (y_mouse - y_offset) * COLUMNS as i32 + (x_mouse - x_offset);
+    let index = (mouse_y - Y_OFFSET) * COLUMNS as i32 + (mouse_x - X_OFFSET);
 
     match tiles.get(index as usize){
         Some(tile) => Ok(tile),
@@ -160,16 +207,58 @@ fn get_tile_from_coordinates(tiles: &Vec<Tile>, y_mouse: i32, x_mouse: i32, y_of
     }
 }
 
+fn get_tile_from_coordinates(tiles: &Vec<Tile>, y: i32, x: i32) -> Result<&Tile, String>{
+    if y < 0 || x < 0 || y >= ROWS as i32 || x >= COLUMNS as i32{
+        return Err("Error: x or y out of bounds".to_string());
+    }
+
+    let index = y * COLUMNS as i32 + x;
+
+    match tiles.get(index as usize){
+        Some(tile) => Ok(tile),
+        None => Err("Error: tile not found at index".to_string())
+    }
+}
+
+fn handle_mouse_input(tiles: &Vec<Tile>, mouse_y: i32, mouse_x: i32) -> Result<String, String>{
+    match get_tile_from_mouse_coordinates(tiles, mouse_y, mouse_x){
+        Ok(tile) => {
+            match tile.get_surrounding_bomb_count(tiles){
+                Some(count) => Ok(format!("Surrounding bomb count: {}", count)),
+                None => Ok("No surrounding bombs found".to_string())
+            }
+        },
+        Err(error) => Err(error)
+    }
+
+}
+
+// didn't make this myself but I understand it and it works so I'm keeping it
+fn assign_symbols_to_all_tiles(tiles: &mut Vec<Tile>) {
+    let symbols: Vec<char> = tiles
+        .iter()
+        // .map() turns every element of the iterator into something else, in this case a char
+        .map(|tile| tile.calculate_symbol(tiles))
+        // .collect() turns the iterator into a vector
+        .collect();
+
+        // .zip() combines two iterators
+    for (tile, symbol) in tiles.iter_mut().zip(symbols) {
+        tile.symbol = symbol;
+    }
+}
 fn main() {
     let mut window = initscr();
     window.keypad(true);
     mousemask(ALL_MOUSE_EVENTS, None); //for pancurses to listen to all mouse events
 
     let mut tiles = create_tiles();
-    conceal_all_tiles(&mut tiles);
-
-    let y_offset = 10;
-    let x_offset = 20;
+    {
+        let tiles_ref = &mut tiles;
+        assign_symbols_to_all_tiles(tiles_ref);
+    }
+    
+    //conceal_all_tiles(&mut tiles);
 
     let mut str_for_debugging = String::new();
 
@@ -181,8 +270,8 @@ fn main() {
         window.mv(0, 0);
         print_manual(&mut window);
 
-        print_tiles(&tiles, &mut window, y_offset, x_offset);
-        print_tile_coordinates(&mut window, y_offset, x_offset);
+        print_tiles(&tiles, &mut window);
+        print_tile_coordinates(&mut window);
 
         window.mvprintw(DEBUG_COORDINATES.0 as i32, DEBUG_COORDINATES.1 as i32, &str_for_debugging);
         window.refresh();
@@ -193,11 +282,11 @@ fn main() {
             }
 
             Ok(InputType::Mouse(y, x)) => {
-                match get_tile_from_coordinates(&tiles, y, x, y_offset, x_offset) {
-                    Ok(tile) => { str_for_debugging = tile.get_symbol().to_string() },
-                    Err(error) => { str_for_debugging = error }
-                    }
+                match handle_mouse_input(&tiles, y, x){
+                    Ok(str) => str_for_debugging = str,
+                    Err(error) => str_for_debugging = error
                 }
+            }
 
             Ok(InputType::Quit) => break,
             
